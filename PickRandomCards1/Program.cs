0ace5764ae7182ea2aa6578c443b90fbd78f2d55
nadiryounes <nadir.younees@gmail.com>
2024-03-27 11:34:11 +0000

    Add project files.

---
+++ PickRandomCards1/Program.cs
@@ -0,0 +1,21 @@
+﻿using PickRandomCards1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of cards to pick: ");
+        string line = Console.ReadLine();
+        if (int.TryParse(line, out int numberOfCards))
+        {
+            foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+            {
+                Console.WriteLine(card);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid number.");
+        }
+    }
+}
